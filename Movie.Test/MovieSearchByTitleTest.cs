11f76623b4030856451fdaf5bac4d03843bee7b4
Kazeem Hassan <kazeem.hassan@outlook.com>
2024-01-26 15:40:38 +0100

    updated

--- Movie.Test/MovieSearchByTitleTest.cs
+++ Movie.Test/MovieSearchByTitleTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Moq;
 using Movies.Application.Implementation;
@@ -17,12 +18,14 @@ namespace Movies.Test
         private readonly Mock<ILogger<MovieSearchService>> _logger;
         private readonly Mock<IConfiguration> _config;
         private readonly Mock<IHttpClientService> _client;
+        private readonly Mock<IMemoryCache> _cache;
 
         public MovieSearchByTitleTest()
         {
             _logger = new Mock<ILogger<MovieSearchService>>();
             _config = new Mock<IConfiguration>();
             _client = new Mock<IHttpClientService>();
+            _cache = new Mock<IMemoryCache>();
         }
 
         [Fact]
@@ -32,7 +35,7 @@ namespace Movies.Test
 
             var searchTitle = string.Empty;
             // Act
-            var service = new MovieSearchService(_logger.Object, _config.Object, _client.Object).GetMoviesByTitle(searchTitle);
+            var service = new MovieSearchService(_logger.Object, _config.Object, _client.Object, _cache.Object).GetMoviesByTitle(searchTitle);
 
             var resp = service.Result;
             // Assert
@@ -60,7 +63,7 @@ namespace Movies.Test
             _client.Setup(c=>c.GetRequest<Data.Movie>(It.IsAny<string>())).Returns(Task.FromResult(new Data.Movie() ));
             // Act
 
-            var service = new MovieSearchService(_logger.Object, _config.Object, _client.Object).GetMoviesByTitle(searchTitle);
+            var service = new MovieSearchService(_logger.Object, _config.Object, _client.Object, _cache.Object).GetMoviesByTitle(searchTitle);
               var resp = service.Result;
             // Assert
 
@@ -71,12 +74,26 @@ namespace Movies.Test
         public void Search_For_Movies_With_Response()
         {
             // Arrange 
+            var searchTitle = "ToyStory";
 
+            var config = new Mock<IConfigurationSection>();
+            config.Setup(c => c.Value).Returns("http://www.omdbapi.com/?apikey=");
 
+            config.Setup(c => c.GetSection("BaseUrl")).Returns(config.Object);
+
+            var configA = new Mock<IConfigurationSection>();
+            configA.Setup(c => c.Value).Returns("1234");
+
+            config.Setup(c => c.GetSection("ApiKey")).Returns(configA.Object);
+
+            _client.Setup(c => c.GetRequest<Data.Movie>(It.IsAny<string>())).Returns(Task.FromResult(new Data.Movie()));
             // Act
 
-
+            var service = new MovieSearchService(_logger.Object, _config.Object, _client.Object, _cache.Object).GetMoviesByTitle(searchTitle);
+            var resp = service.Result;
             // Assert
+
+            Assert.Equal(ResponseCodesEnum.Success, resp.responseCode); 
 
 
         }
